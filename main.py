d765618b096671632140a8a50b9dbd855cf99cc0
Gokul Ganesh I <igokulganesh@gmail.com>
2020-11-25 13:09:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-from flask import Flask
-app = Flask(__name__)
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+import os
+from sklearn.externals import joblib
+import pickle
 
+#Initialize the flask App
+app = Flask(__name__)
+model = pickle.load(open('model.pickle', 'rb'))
+
+#default page of our web-app
 @app.route('/')
-def hello_world():
-   return 'Hello hello_world'
+def home():
+    return render_template('main.html')
+
+#To use the predict button in our web-app
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='CO2 Emission of the vehicle is :{}'.format(output))
 
 if __name__ == '__main__':
 	app.debug = True
