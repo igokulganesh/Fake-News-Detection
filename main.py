04a148164ce7c3b688014737758df6afd6c664f2
Gokul Ganesh I <igokulganesh@gmail.com>
2020-11-26 13:44:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template
-#from flask_cors import CORS
+from flask import Flask, request,render_template
+from flask_cors import CORS
 import os
 #from sklearn.externals import joblib
+import sklearn
 import pickle
+import flask
+import os
 import newspaper
 from newspaper import Article
 import urllib
 import nltk
+nltk.download('punkt')
 
-#Initialize the flask App
+#Loading Flask and assigning the model variable
 app = Flask(__name__)
-model = pickle.load(open('model.pickle', 'rb'))
+CORS(app)
+app=flask.Flask(__name__,template_folder='templates')
+
+with open('model.pickle', 'rb') as handle:
+    model = pickle.load(handle)
 
 #default page of our web-app
 @app.route('/')
@@ -21,24 +29,17 @@ def home():
 #To use the predict button in our web-app
 @app.route('/predict',methods=['GET','POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
     title=request.get_data(as_text=True)
     url=request.get_data(as_text=True)[5:]
     url=urllib.parse.unquote(url)
     article=Article(str(url))
-    
     article.download()
-    while article.download_state==0:
-        time.sleep(1)
     article.parse()
     article.nlp()
 
     news = article.summary
     head = article.title
-    pred=model.predict([head, news])
-    
+    pred = model.predict([[head, news]])
 
     return render_template('main.html', prediction_text='The news is :{}'.format(pred[0]))
 
